f8cd7775fcdd8907a56a6c0029da4da8b857af1b
owais abu-kreifeh <owais.majid@yahoo.com>
2017-06-19 08:36:06 +0300

    stdIO Example and gitignore file

---
+++ stdIO.kt
@@ -0,0 +1,22 @@
+/**
+* Created by Owais Majid on 6/19/2017.
+*/
+/*
+* To read from Console use : readLine()
+* ex : var str = readLine()
+*
+* To print things on Console use : println("Some Text")
+* ex : var txt = "Hello World"
+*      println(txt)
+*/
+fun main (p: Array<String>){
+    println("Enter Your first name:")
+    val firstName = readLine()
+    println("Enter Your last name:")
+    val lastName = readLine()
+
+    println("Your full name: $firstName $lastName")
+
+    //val x= readLine()
+    //println(x)
+}
